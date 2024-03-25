07be6b48de1374e960ab1307e3c01ca81707823b
likhdima@yandex.ru <likhdima@yandex.ru>
2024-03-25 23:05:43 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApp13/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp13
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = @" F:\Temp";
+            string[] files = Directory.GetFiles(path);
+            foreach (string file in files)
+            {
+                Console.WriteLine(file);
+            }
+            Console.ReadKey();
+        }
+    }
+}
+
+
+        
+    
+
